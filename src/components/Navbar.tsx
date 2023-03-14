cb5a92c69cd9407e8c6f6d9eba564ebf8fd59b0b
Jaz <pjaziel723@gmail.com>
2023-03-14 03:29:26 -0600

    upgrade Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,12 @@
- import React, { useState, useEffect } from "react";
+
+import React, { useState } from "react";
+import Link from "next/link";
 import { motion } from "framer-motion";
-import NextLink from "next/link";
+import { FaBars } from "react-icons/fa";
+import Image from "next/image";
 
 const Navbar: React.FC = () => {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const navbarVariants = {
     hidden: {
@@ -37,79 +36,72 @@ const Navbar: React.FC = () => {
         duration: 0.5,
       },
     },
+    hover: {
+      scale: 1.09,
+      transition: {
+        duration: 0.8,
+      },
+    },
   };
-
-  if (!mounted) {
-    return null;
-  }
 
   return (
     <motion.nav
-      className="bg-white shadow"
+      className="bg-gradient-to-r from-blue-900 to-white text-black"
       variants={navbarVariants}
       initial="hidden"
       animate="visible"
     >
-      <div className="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center">
+      <div className="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center md:justify-center">
         <motion.div
           className="flex items-center justify-between"
           variants={linkVariants}
         >
-          <NextLink href="#slide" passHref>
-            <motion.a
-              className="font-bold text-xl tracking-tight text-gray-800 uppercase"
-              whileHover={{ scale: 1.05 }}
-            >
-              Logo
-            </motion.a>
-          </NextLink>
-          <motion.button
-            className="border border-gray-800 px-3 py-2 rounded text-gray-800 opacity-50 hover:opacity-75 md:hidden"
-            type="button"
-            whileHover={{ scale: 1.05 }}
-          >
-            <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-              <path
-                fillRule="evenodd"
-                d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
+          <Link href="/">
+            <div className="flex items-center mr-4">
+              <Image
+                src="/images/logos/logoslideuno.svg"
+                alt="imagen-logo"
+                width={50}
+                height={50}
+                quality={100}
               />
-            </svg>
-          </motion.button>
+            </div>
+          </Link>
+          <button
+            className=" md:hidden text-white focus:outline-none text-black"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <FaBars />
+          </button>
         </motion.div>
+		  <div className="flex justify-center">
         <motion.div
-          className="md:flex items-center"
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } md:flex md:items-center`}
           variants={linkVariants}
         >
-          <NextLink href="/" passHref>
-            <motion.a
-              className="block mt-4 md:inline-block md:mt-0 mr-6 font-semibold text-gray-800 hover:text-gray-900"
-              whileHover={{ scale: 1.05 }}
-            >
-              Home
-            </motion.a>
-          </NextLink>
-          <NextLink href="/projects" passHref>
-            <motion.a
-              className="block mt-4 md:inline-block md:mt-0 mr-6 font-semibold text-gray-800 hover:text-gray-900"
-              whileHover={{ scale: 1.05 }}
-            >
-              Projects
-            </motion.a>
-          </NextLink>
-          <NextLink href="/contact" passHref>
-            <motion.a
-              className="block mt-4 md:inline-block md:mt-0 font-semibold text-gray-800 hover:text-gray-900"
-              whileHover={{ scale: 1.05 }}
-            >
-              Contact
-
-
-            </motion.a>
-          </NextLink>
+          <Link href="/">
+            <div className="block mt-4 md:inline-block md:mt-0 mr-6 font-semibold">
+              Inicio
+            </div>
+          </Link>
+          <Link href="/proyectos">
+            <div className="block mt-4 md:inline-block md:mt-0 mr-6 font-semibold">
+              Proyectos
+            </div>
+          </Link>
+          <Link href="/contacto">
+            <div className="block mt-4 md:inline-block md:mt-0 ">
+              Contacto
+            </div>
+          </Link>
         </motion.div>
+			</div>
       </div>
     </motion.nav>
   );
 };
 
 export default Navbar;
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { Inter } from 'next/font/google'
 import Hero from '../components/HeroCar';
 import Navbar from '../components/Navbar';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
